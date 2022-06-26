cbb93df93fcdceae611382de1b33f5ae96e90e4c
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-06-26 13:03:26 +0530

    Time: 3 ms (44.16%), Space: 7.1 MB (21.87%) - LeetHub

---
+++ 671-second-minimum-node-in-a-binary-tree/671-second-minimum-node-in-a-binary-tree.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findSecondMinimumValue(TreeNode* root) {
+        
+        // vector<int>v;
+        // preorder(root,v);
+        
+        //preorder(root,m);
+        map<int,int>m;
+        vector<int>v;
+        preorder(root,m);
+        
+        int min_val=INT_MAX;
+        int second_min_val=INT_MAX;
+        for(auto x:m){
+            min_val=min(min_val,x.first);
+        }
+        
+        // if(m.size()==2){
+        //    if(m[min_val]>1)return min_val;
+        // }
+        
+        for(auto x:m){
+            if(x.first!=min_val)second_min_val=min(second_min_val,x.first);
+        } 
+        
+        return (m.size()>1)?second_min_val:-1;
+    }
+    
+    void preorder(TreeNode* root,map<int,int>&m){
+        
+        if(!root)return;
+        
+        m[root->val]++;
+        preorder(root->left,m);
+        preorder(root->right,m);
+    }
+    
+    
+};
