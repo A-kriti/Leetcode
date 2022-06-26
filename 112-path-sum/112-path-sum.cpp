df13ab78f45382299e43cf2000972695058c2c16
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-06-26 11:18:22 +0530

    Time: 23 ms (21.98%), Space: 21.3 MB (39.62%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -11,36 +11,51 @@
  */
 class Solution {
 public:
-    bool hasPathSum(TreeNode* root, int targetSum) {
+       bool hasPathSum(TreeNode* root, int targetSum, int c=0) {
+           
+           if(!root)return false;
+           
+           c+=root->val;
+           if(root->left==NULL && root->right==NULL){
+               if(c==targetSum)return true;
+           }
+           
+           return hasPathSum(root->left,targetSum,c) || hasPathSum(root->right,targetSum,c);
+           
+       }
+             
+             
+//     bool hasPathSum(TreeNode* root, int targetSum) {
         
-        int sum=0;
-        vector<int>v;
+//         int sum=0;
+//         vector<int>v;
         
-        preorder(root,v,sum);
+//         preorder(root,v,sum);
         
-        for(int x:v){
-            if(x==targetSum)return true;
-        }
+//         for(int x:v){
+//             if(x==targetSum)return true;
+//         }
         
-        return false;
+//         return false;
         
-    }
+//     }
     
-    void preorder(TreeNode* root,vector<int> &v,int sum){
+//     void preorder(TreeNode* root,vector<int> &v,int sum){
         
-        if(!root){
+//         if(!root){
             
-            return;
-        }
+//             return;
+//         }
         
-        sum+=root->val;
+//         sum+=root->val;
         
-        if(root->left==NULL && root->right==NULL){
-            v.push_back(sum);
-            return;
-        }
+//         if(root->left==NULL && root->right==NULL){
+//             v.push_back(sum);
+//             return;
+//         }
         
-        preorder(root->left,v,sum);
-        preorder(root->right,v,sum);
-    }
+//         preorder(root->left,v,sum);
+//         preorder(root->right,v,sum);
+//     }
+    
 };
