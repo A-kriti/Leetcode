cd3c5d2e6499e90eaf0e972642cca98aa6c29f93
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-06-26 12:05:57 +0530

    Time: 27 ms (32.61%), Space: 37.2 MB (28.00%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -16,33 +16,31 @@ public:
         vector<vector<int>>v;
         vector<vector<int>>v1;
         vector<int>t;
-        preorder(root,v,t);
-        
-        int n=v.size();
-        
-        for(int i=0;i<n;i++){
-            int c=0;
-            for(int j=0;j<v[i].size();j++){
-                c+=v[i][j];
-            }
-            if(c==targetSum)v1.push_back(v[i]);
-        }
-        
-        return v1;
+        preorder(root,v,t,targetSum);
+        return v;
     }
     
-    void preorder(TreeNode* root,vector<vector<int>>&v,vector<int>t){
+    void preorder(TreeNode* root,vector<vector<int>>&v,vector<int>t,int targetSum){
+        
         
         if(!root)return;
         
         t.push_back(root->val);
         
+        
         if(root->left==NULL && root->right==NULL){
-            v.push_back(t);
+            
+            int c=0;
+            for(int j=0;j<t.size();j++){
+                c+=t[j];
+            }
+            
+            if(c==targetSum)v.push_back(t);
             return;
         }
+       
+        preorder(root->left,v,t,targetSum);
+        preorder(root->right,v,t,targetSum);
         
-        preorder(root->left,v,t);
-        preorder(root->right,v,t);
     }
 };
