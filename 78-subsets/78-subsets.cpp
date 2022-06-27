d39e30370d9175ff3a02b0a728d7e39644510b50
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-06-27 12:30:20 +0530

    Time: 3 ms (71.53%), Space: 6.9 MB (95.76%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -12,16 +12,44 @@ public:
     
     void generate(vector<int>& nums,int i,vector<int>&subset,vector<vector<int>>&ans){
         
-        if(i==nums.size()){
-            ans.push_back(subset);
-            return;
+        ans.push_back(subset);
+        
+        for(int j=i;j<nums.size();j++){
+            
+            subset.push_back(nums[j]);
+            generate(nums,j+1,subset,ans);
+            subset.pop_back();
         }
         
-        //exclude nums[i]
-        generate(nums,i+1,subset,ans);
-        subset.push_back(nums[i]);
-        //include nums[i]
-        generate(nums,i+1,subset,ans);
-        subset.pop_back();
     }
 };
+
+// class Solution {
+// public:
+//     vector<vector<int>> subsets(vector<int>& nums) {
+        
+//         sort(nums.begin(),nums.end());
+//         vector<int>subset;
+//         vector<vector<int>>ans;
+//         int i=0;
+//         generate(nums,i,subset,ans);
+        
+//         return ans;
+//     }
+    
+//     void generate(vector<int>& nums,int i,vector<int>&subset,vector<vector<int>>&ans){
+        
+//         if(nums[i]==nums[i+1])continue;
+//         if(i==nums.size()){
+//             ans.push_back(subset);
+//             return;
+//         }
+        
+//         //exclude nums[i]
+//         generate(nums,i+1,subset,ans);
+//         subset.push_back(nums[i]);
+//         //include nums[i]
+//         generate(nums,i+1,subset,ans);
+//         subset.pop_back();
+//     }
+// };
