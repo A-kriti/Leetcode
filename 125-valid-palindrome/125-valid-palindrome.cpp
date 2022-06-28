e11feeecabdd6ba5027797e5e27b3ecc1475eb10
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-06-28 12:07:18 +0530

    Time: 14 ms (17.34%), Space: 7.9 MB (7.38%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,61 +1,62 @@
-class Solution {
-public:
+//method 1
+// class Solution {
+// public:
     
-    bool ischar(char x){
+//     bool ischar(char x){
         
-        int t=int(x);
-        if(t>=97 && t<=122 || t>=65 && t<=90 || t>=48 && t<=57){
-            return true;
-        }
+//         int t=int(x);
+//         if(t>=97 && t<=122 || t>=65 && t<=90 || t>=48 && t<=57){
+//             return true;
+//         }
         
-        return false;
+//         return false;
         
-    }
+//     }
     
-    bool isPalindrome(string s) {
+//     bool isPalindrome(string s) {
         
-        int n=s.length();
-        string r="";
+//         int n=s.length();
+//         string r="";
         
-        for(int i=0;i<n;i++){
+//         for(int i=0;i<n;i++){
             
-            if(ischar(s[i])){
-                r+=tolower(s[i]);
-            }
+//             if(ischar(s[i])){
+//                 r+=tolower(s[i]);
+//             }
               
-        }
+//         }
         
         
         
         
-        for(int i=0;i<r.size()/2;i++){
+//         for(int i=0;i<r.size()/2;i++){
             
-            if(r[i]!=r[r.size()-1-i]){
-                return false;
-            }
-        }
+//             if(r[i]!=r[r.size()-1-i]){
+//                 return false;
+//             }
+//         }
         
-        return true;
-    }
-};
+//         return true;
+//     }
+// };
 
 //method 2
 
-// class Solution {
-// public:
-//     bool isPalindrome(string s) {
+class Solution {
+public:
+    bool isPalindrome(string s) {
         
-//         string t="";
-//         int n=s.size();
+        string t="";
+        int n=s.size();
         
-//         for(char x:s){
+        for(char x:s){
             
-//             if(isalpha(x) || isdigit(x))(t+=tolower(x));
-//         }
+            if(isalpha(x) || isdigit(x))(t+=tolower(x));
+        }
         
-//         string g=t;
-//         reverse(g.begin(),g.end());
+        string g=t;
+        reverse(g.begin(),g.end());
         
-//         return t==g;
-//     }
-// };
+        return t==g;
+    }
+};
