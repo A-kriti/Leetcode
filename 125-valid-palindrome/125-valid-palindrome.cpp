4be39ac2d0f25b52c66f8efb435c53ca776d58c8
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-06-28 12:06:51 +0530

    Time: 15 ms (14.40%), Space: 7.7 MB (22.85%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,18 +1,61 @@
 class Solution {
 public:
-    bool isPalindrome(string s) {
+    
+    bool ischar(char x){
         
-        string t="";
-        int n=s.size();
-        
-        for(char x:s){
-            
-            if(isalpha(x) || isdigit(x))(t+=tolower(x));
+        int t=int(x);
+        if(t>=97 && t<=122 || t>=65 && t<=90 || t>=48 && t<=57){
+            return true;
         }
         
-        string g=t;
-        reverse(g.begin(),g.end());
+        return false;
         
-        return t==g;
+    }
+    
+    bool isPalindrome(string s) {
+        
+        int n=s.length();
+        string r="";
+        
+        for(int i=0;i<n;i++){
+            
+            if(ischar(s[i])){
+                r+=tolower(s[i]);
+            }
+              
+        }
+        
+        
+        
+        
+        for(int i=0;i<r.size()/2;i++){
+            
+            if(r[i]!=r[r.size()-1-i]){
+                return false;
+            }
+        }
+        
+        return true;
     }
 };
+
+//method 2
+
+// class Solution {
+// public:
+//     bool isPalindrome(string s) {
+        
+//         string t="";
+//         int n=s.size();
+        
+//         for(char x:s){
+            
+//             if(isalpha(x) || isdigit(x))(t+=tolower(x));
+//         }
+        
+//         string g=t;
+//         reverse(g.begin(),g.end());
+        
+//         return t==g;
+//     }
+// };
