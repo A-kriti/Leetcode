debff124b1db301c3f782a7796ed36f46c21eef2
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-06-29 10:07:39 +0530

    Added solution - LeetHub

--- Kth smallest element - GFG/kth-smallest-element.cpp
+++ Kth smallest element - GFG/kth-smallest-element.cpp
@@ -14,37 +14,59 @@ class Solution{
     // r : ending index of the array i.e size-1
     // k : find kth smallest element and return using this function
     
-    vector<int> sort(int arr[],int n){
-        
-        int c[100005];
-        vector<int>v;
-        
-        for(int i=0;i<100000;i++){
-            c[i]=0;
-        }
-        
-        for(int i=0;i<n;i++){
-            c[arr[i]]++;
-        }
-        
-        for(int i=0;i<100000;i++){
-            if(c[i]>0){
-                for(int j=0;j<c[i];j++){
-                    v.push_back(i);
-                }
-            }
-        }
-        return v;
-    }
+    
     
     int kthSmallest(int arr[], int l, int r, int k) {
-        //code here
-        //int n=sizeof(arr)/sizeof(arr[0]);
-        vector<int>v=sort(arr,r+1);
         
-        return v[k-1];
+        priority_queue<int>p;
+        
+        for(int i=0;i<=r;i++){
+            p.push(arr[i]);
+            if(p.size()>k)p.pop();
+        }
+        
+        return p.top();
     }
 };
+
+// class Solution{
+//     public:
+//     // arr : given array
+//     // l : starting index of the array i.e 0
+//     // r : ending index of the array i.e size-1
+//     // k : find kth smallest element and return using this function
+    
+//     vector<int> sort(int arr[],int n){
+        
+//         int c[100005];
+//         vector<int>v;
+        
+//         for(int i=0;i<100000;i++){
+//             c[i]=0;
+//         }
+        
+//         for(int i=0;i<n;i++){
+//             c[arr[i]]++;
+//         }
+        
+//         for(int i=0;i<100000;i++){
+//             if(c[i]>0){
+//                 for(int j=0;j<c[i];j++){
+//                     v.push_back(i);
+//                 }
+//             }
+//         }
+//         return v;
+//     }
+    
+//     int kthSmallest(int arr[], int l, int r, int k) {
+//         //code here
+//         //int n=sizeof(arr)/sizeof(arr[0]);
+//         vector<int>v=sort(arr,r+1);
+        
+//         return v[k-1];
+//     }
+// };
 
 // { Driver Code Starts.
  
