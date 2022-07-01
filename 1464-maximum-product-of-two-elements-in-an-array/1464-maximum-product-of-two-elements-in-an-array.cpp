7c307643e043793f19a8dbba0e82162326fe736b
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-07-01 11:41:58 +0530

    Time: 5 ms (80.30%), Space: 9.9 MB (65.01%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -2,15 +2,28 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
        
-        int n=nums.size();
-        int max1=n-1;
-        int max2=n-1;
+//         int n=nums.size();
+//         int max1=n-1;
+//         int max2=n-1;
         
-        for(int i=0;i<n;i++){
+//         for(int i=0;i<n;i++){
+//             if(nums[i]>nums[max1])max1=i;
+//         }
+//         for(int i=0;i<n;i++){
+//             if(i!=max1 && (max2==n-1 || nums[i]>nums[max2]))max2=i;
+//         }
+        
+//         return (nums[max1]-1)*(nums[max2]-1);
+        
+        int n=nums.size();
+        int max1=0;
+        int max2=0;
+        
+        for(int i=1;i<n;i++){
             if(nums[i]>nums[max1])max1=i;
         }
-        for(int i=0;i<n;i++){
-            if(i!=max1 && (max2==n-1 || nums[i]>nums[max2]))max2=i;
+        for(int i=1;i<n;i++){
+            if(i!=max1 && (max2==0 || nums[i]>nums[max2]))max2=i;
         }
         
         return (nums[max1]-1)*(nums[max2]-1);
