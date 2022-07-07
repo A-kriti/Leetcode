72e561f8b12b149a58ac0b8c0bd9d0d79e58a4ca
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-07-07 10:41:03 +0530

    Added solution - LeetHub

--- IPL 2021 - Match Day 2 - GFG/ipl-2021-match-day-2.cpp
+++ IPL 2021 - Match Day 2 - GFG/ipl-2021-match-day-2.cpp
@@ -8,29 +8,18 @@ class Solution {
   public:
     vector<int> max_of_subarrays(vector<int> arr, int n, int k) {
         // your code here
-        // priority_queue<int>p;
-        // vector<int>ans;
         
-        // for(int x:arr){
-        //     p.push(x);
-        //     if(p.size()==k){
-        //         ans.push_back(p.top());
-        //         p.pop();
-        //     }
-            
+        
+        // if(k==1)return arr;
+        // if(n<=k){
+        //     int t=*max_element(arr.begin(),arr.end());
+        //     return {t};
         // }
-        
-        if(k==1)return arr;
-        if(n<=k){
-            int t=*max_element(arr.begin(),arr.end());
-            return {t};
-        }
         vector<int>ans;
         list<int>l;
         int i=0;
         int j=0;
-        //v.push_back(arr[i]);
-        int max_el=arr[i];
+
         
         while(j<n){
             
@@ -39,24 +28,14 @@ class Solution {
             }
             l.push_back(arr[j]);
             
-            // max_el=max(max_el,arr[j]);
-            // v.push_back(arr[j]);
-            
             if(j-i+1==k){
-                //max_el=*max_element(v.begin()+i,v.begin()+j+1);
+              
                 ans.push_back(l.front());
                 if(l.front()==arr[i]){
-                    
-                    // int k=v.size();
-                    // while(k--){
-                    //     max_el=max(max_el,arr[k]);
-                    // }
+                
                     l.pop_front();
                 }
-                // else if(!v.empty()){
-                //     v.erase(v.begin()+i);
-                // }
-                //v.erase(v.begin()+i);
+                
                 i++;
             }
             j++;
