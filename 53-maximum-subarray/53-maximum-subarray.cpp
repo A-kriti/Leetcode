888f356342d2819d745a11ba9a41169b29fa5e64
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-07-10 06:38:39 +0530

    Time: 124 ms (87.31%), Space: 67.8 MB (10.89%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         
+        //kadane's Alogorithm
         int max_sum=INT_MIN;
         int sum=0;
         
@@ -14,5 +15,27 @@ public:
         }
         
         return max_sum;
+        
+        // O(n^2) approach using prefix sum but it throws TLE
+//         int max_sum=INT_MIN;
+//         int sum=0;
+//         int n=nums.size();
+        
+//         int curr[n+1];
+//         curr[0]=0;
+        
+//         for(int i=1;i<=n;i++){
+//             curr[i]=curr[i-1]+nums[i-1];
+//         }
+        
+//         for(int i=1;i<=n;i++){
+//             sum=0;
+//             for(int j=0;j<i;j++){
+//                 sum=curr[i]-curr[j];
+//                 max_sum=max(max_sum,sum);
+//             }
+//         }
+        
+//         return max_sum;
     }
 };
