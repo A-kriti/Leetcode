d859b8baee950c3f7851a748b3fc6586562ce5e4
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-07-18 10:16:27 +0530

    Time: 3 ms (73.21%), Space: 7.1 MB (87.22%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,55 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        
-        vector<int>subset;
-        vector<vector<int>>ans;
-        int i=0;
-        generate(nums,i,subset,ans);
-        
-        return ans;
-    }
-    
-    void generate(vector<int>& nums,int i,vector<int>&subset,vector<vector<int>>&ans){
-        
+    vector<vector<int>>ans;
+    void genrate(vector<int>& nums,vector<int>&subset,int c){
         ans.push_back(subset);
         
-        for(int j=i;j<nums.size();j++){
-            
-            subset.push_back(nums[j]);
-            generate(nums,j+1,subset,ans);
+        for(int i=c;i<nums.size();i++){
+            subset.push_back(nums[i]);
+            genrate(nums,subset,i+1);
             subset.pop_back();
         }
-        
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int>subset;
+        int c=0;
+        genrate(nums,subset,c);
+        return ans;
     }
 };
-
-// class Solution {
-// public:
-//     vector<vector<int>> subsets(vector<int>& nums) {
-        
-//         sort(nums.begin(),nums.end());
-//         vector<int>subset;
-//         vector<vector<int>>ans;
-//         int i=0;
-//         generate(nums,i,subset,ans);
-        
-//         return ans;
-//     }
-    
-//     void generate(vector<int>& nums,int i,vector<int>&subset,vector<vector<int>>&ans){
-        
-//         if(nums[i]==nums[i+1])continue;
-//         if(i==nums.size()){
-//             ans.push_back(subset);
-//             return;
-//         }
-        
-//         //exclude nums[i]
-//         generate(nums,i+1,subset,ans);
-//         subset.push_back(nums[i]);
-//         //include nums[i]
-//         generate(nums,i+1,subset,ans);
-//         subset.pop_back();
-//     }
-// };
