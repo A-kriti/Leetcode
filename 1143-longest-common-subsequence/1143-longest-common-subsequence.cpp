1b0a22fb5b1d78dd3f93b3f0a8cc9ca06d4dbee2
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-07-18 10:32:23 +0530

    Time: 50 ms (25.34%), Space: 10.8 MB (65.69%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
+    int t[1001][1001];
     
-    int t[1000+1][1000+1];
     int lcs(string &text1, string &text2,int n,int m){
         
-        if(m<0 || n<0){
-            return 0;
-        }
+        if(m<0 || n<0)return 0;
         
-        if(t[n][m]!=-1){
-            return t[n][m];
-        }
+        if(t[n][m]!=-1)return t[n][m];
         
         if(text1[n]==text2[m]){
             return 1+lcs(text1,text2,n-1,m-1);
         }
         
-        return t[n][m] = max(lcs(text1,text2,n-1,m),lcs(text1,text2,n,m-1));
-        
-        //return t[n][m];
+        return t[n][m]=max(lcs(text1,text2,n-1,m),lcs(text1,text2,n,m-1));
     }
+    
     int longestCommonSubsequence(string text1, string text2) {
         
         int n=text1.size();
         int m=text2.size();
-        
-//         for(int i=0;i<n;i++){
-//             for(int j=0;j<m;j++){
-//                 t[i][j]=-1;
-//             }
-//         }
-        memset(t, -1, sizeof(t));
-        
+        memset(t,-1,sizeof(t));
         int ans=lcs(text1,text2,n-1,m-1);
-        return ans;
         
+        return ans;
     }
 };
