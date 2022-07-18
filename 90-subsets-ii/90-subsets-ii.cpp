e1b557493b952454df75a8dcb78043eee053ead9
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-07-18 10:21:46 +0530

    Time: 10 ms (31.90%), Space: 7.6 MB (73.06%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        
-        sort(nums.begin(),nums.end());
-        vector<int>subset;
-        vector<vector<int>>ans;
-        int i=0;
-        genrate(nums,subset,ans,i);
-        
-        
-        return ans;
-    }
     
-    void genrate(vector<int>& nums,vector<int>& subset,vector<vector<int>>& ans,int i){
-      
-        ans.push_back(subset);  
-        for(int j=i;j<nums.size();j++){
-            
-            if(i!=j && nums[j]==nums[j-1])continue;
-            subset.push_back(nums[j]);
-            genrate(nums,subset,ans,j+1);
+    vector<vector<int>>ans;
+    void genrate(vector<int>& nums,vector<int>&subset,int c){
+        ans.push_back(subset);
+        
+        for(int i=c;i<nums.size();i++){
+            if(i!=c && nums[i]==nums[i-1])continue;
+            subset.push_back(nums[i]);
+            genrate(nums,subset,i+1);
             subset.pop_back();
         }
-            
+    }
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<int>subset;
+        int c=0;
+        genrate(nums,subset,c);
+        return ans;
     }
 };
