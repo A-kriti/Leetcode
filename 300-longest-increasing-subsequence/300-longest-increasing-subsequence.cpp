e36cdcee0b7ae8bb52a146d7def23534615b4a8e
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-07-20 10:01:55 +0530

    Time: 3 ms (99.78%), Space: 10.4 MB (87.63%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -36,3 +36,29 @@ public:
         return len;
     }
 };
+
+
+//method 2
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+        
+//         int n=nums.size();
+//         int v[n+1];
+//         v[0]=1;
+        
+//         for(int i=1;i<n;i++){
+//             v[i]=1;
+//             for(int j=0;j<i;j++){
+//                 if(nums[j]<nums[i])
+//                     v[i]=max(v[i],v[j]+1);
+//             }
+//         }
+        
+//         int m=INT_MIN;
+//         for(int i=0;i<n;i++){
+//             m=max(m,v[i]);
+//         }
+//         return m;
+//     }
+// };
