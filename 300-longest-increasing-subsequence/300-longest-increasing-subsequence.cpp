ee0337f88694073733edfa2a16e86373a2ce2017
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-07-20 10:00:00 +0530

    Time: 10 ms (91.87%), Space: 10.4 MB (87.63%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,23 +1,45 @@
 class Solution {
 public:
+    
+    int ceil_index(int v[],int r,int x){
+        
+        
+        int l=0;
+        
+        while(l<r){
+            int m=l+(r-l)/2;
+            
+            if(v[m]>=x){
+                r=m;
+            }
+            else l=m+1;
+        }
+        
+        return r;
+    }
     int lengthOfLIS(vector<int>& nums) {
         
         int n=nums.size();
-        int v[n+1];
-        v[0]=1;
+        int v[n];
+        v[0]=nums[0];
+        int len=1;
         
         for(int i=1;i<n;i++){
-            v[i]=1;
-            for(int j=0;j<i;j++){
-                if(nums[j]<nums[i])
-                    v[i]=max(v[i],v[j]+1);
+            if(nums[i]>v[len-1]){
+                v[len]=nums[i];
+                len++;
+            }
+            
+            else{
+                int x=ceil_index(v,len-1,nums[i]);
+                v[x]=nums[i];
             }
         }
         
-        int m=INT_MIN;
-        for(int i=0;i<n;i++){
-            m=max(m,v[i]);
-        }
-        return m;
+        // int m=INT_MIN;
+        // for(int i=0;i<n;i++){
+        //     m=max(m,v[i]);
+        // }
+        return len;
     }
 };
