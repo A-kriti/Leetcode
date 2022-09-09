07aa2a751b2eee30029b16d5bf270bfea5def95c
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-09-09 16:54:03 +0530

    Time: 35 ms (34.16%), Space: 14.9 MB (43.19%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
+    
     vector<vector<int>>ans;
     
-    void fun(vector<int>& candidates, int target,vector<int>v,int i){
+    void fucn(vector<int>& candidates, int target,vector<int>v,int i){
         
+        int n=candidates.size();
         if(target==0){
-            
             ans.push_back(v);
+            return;
         }
         
-        while(i<candidates.size() && target-candidates[i]>=0){
+        while(i<n && target-candidates[i]>=0){
             
-                v.push_back(candidates[i]);
-                fun(candidates,target-candidates[i],v,i);
-                i++;
-                v.pop_back();
-            
+            v.push_back(candidates[i]);
+            fucn(candidates,target-candidates[i],v,i);
+            i++;
+            v.pop_back();
         }
+        
     }
-    
+        
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         
-        sort(candidates.begin(),candidates.end());
         vector<int>v;
-        fun(candidates,target,v,0);
+        sort(candidates.begin(),candidates.end());
         
+        fucn(candidates,target,v,0);
         return ans;
     }
 };
