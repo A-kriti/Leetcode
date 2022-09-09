f3667517bc9c7e7fc096169b8015f6034b276961
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-09-09 23:22:38 +0530

    Time: 7 ms (35.94%), Space: 7.5 MB (49.79%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
+    
     vector<vector<int>>ans;
-    void genrate(vector<int>& nums,vector<int>&subset,int c){
-        ans.push_back(subset);
+    
+    void genrate(vector<int>& nums,vector<int>v,int l){
         
-        for(int i=c;i<nums.size();i++){
-            subset.push_back(nums[i]);
-            genrate(nums,subset,i+1);
-            subset.pop_back();
+        ans.push_back(v);
+        int n=nums.size();
+        
+        for(int i=l;i<n;i++){
+            v.push_back(nums[i]);
+            genrate(nums,v,i+1);
+            v.pop_back();
         }
+        
     }
     
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int>subset;
-        int c=0;
-        genrate(nums,subset,c);
+        
+        vector<int>v;
+        genrate(nums,v,0);
         return ans;
     }
 };
