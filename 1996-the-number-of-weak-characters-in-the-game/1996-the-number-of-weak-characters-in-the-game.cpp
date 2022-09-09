569a711da6d0c48c89cdaf44192dc2a16efee842
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-09-09 08:06:03 +0530

    Time: 1552 ms (10.35%), Space: 125 MB (98.94%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
-    static bool comp(vector<int>& a,vector<int>& b){
-			if(a[0]==b[0])return a[1]>b[1];
-			return a[0]<b[0];
-		}
+    
+    static bool cmp(vector<int>&a,vector<int>&b){
+
+        if(a[0]==b[0]){
+            return a[1]>b[1];
+        }
+        
+        return b[0]>a[0];
+    }
+    
     int numberOfWeakCharacters(vector<vector<int>>& properties) {
+        
+        // 3 2,3 6, 5 5, 6 3
+        // 4 3, 1 5, 10 4
+        // 1 2, 4 3, 7 3, 1 5, 1 5, 7 7, 9 7, 9 8, 3 10, 8 10
+        // 1 2, 1 5, 1 5, 3 10, 4 3, 7 3, 7 7, 8 10, 9 7, 9 8
         int n=properties.size();
-			sort(properties.begin(),properties.end(),comp);
-			int maxdef=INT_MIN;
-			int count=0;
-			for(int i=n-1;i>=0;i--){
-				if(properties[i][1]<maxdef) count++;
-				maxdef=max(maxdef,properties[i][1]);
-			}
-			return count;
+        
+        sort(properties.begin(),properties.end(),cmp);
+        
+        int c=0;
+       
+        int x=INT_MIN;
+        
+        for(int i=n-1;i>=0;i--){
+            
+            if(properties[i][1]<x){
+                c++;    
+            }
+            x=max(x,properties[i][1]);
+        }
+        
+        return c;
     }
 };
