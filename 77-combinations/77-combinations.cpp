2c848da41fd51e20b13578bda2af6e7ddf76976c
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-09-09 23:32:09 +0530

    Time: 1032 ms (5.00%), Space: 165 MB (8.01%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     
     vector<vector<int>>ans;
-    void genrate(vector<int>&nums,vector<int>v,int k,int l){
+    void genrate(int n,vector<int>v,int k,int l){
         
         if(v.size()==k){
             ans.push_back(v);
             return;
         }
         
-        int n=nums.size();
-        for(int i=l;i<n;i++){
-            v.push_back(nums[i]);
-            genrate(nums,v,k,i+1);
+        for(int i=l;i<=n;i++){
+            v.push_back(i);
+            genrate(n,v,k,i+1);
             v.pop_back();
             
         }
@@ -20,13 +19,8 @@ public:
     
     vector<vector<int>> combine(int n, int k) {
         
-        vector<int>nums;
         vector<int>v;
-        for(int i=1;i<=n;i++){
-            nums.push_back(i);
-        }
-        
-        genrate(nums,v,k,0);
+        genrate(n,v,k,1);
         return ans;
     }
 };
