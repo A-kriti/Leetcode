d8e10fc1806bc79cbf63733739d698b96f79a0fb
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-09-17 11:53:51 +0530

    Time: 63 ms (10.18%), Space: 16.3 MB (12.92%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -20,48 +20,7 @@ public:
             genrate(candidates,target-candidates[i],v,i+1);
             v.pop_back();
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//         int n=candidates.size();
-//         if(target==0){
-//             ans.push_back(v);
-//             return;
-//         }
-        
-//         if(target<0)return;
-        
-//         for(int i=l;i<n;i++){
-            
-//             if(i!=l && candidates[i]==candidates[i-1])continue;
-//             v.push_back(candidates[i]);
-//             genrate(candidates,target-candidates[i],v,i+1);
-//             v.pop_back();
-//         }
+    
     }
     
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
