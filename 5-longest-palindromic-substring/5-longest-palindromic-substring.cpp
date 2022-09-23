f81383bed7e7f0685e2fa02775b6cac50c3f99dd
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-09-23 15:05:29 +0530

    Time: 303 ms (34.89%), Space: 23.7 MB (36.47%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -15,11 +15,14 @@ public:
         
         for(int dif=0;dif<n;dif++){
             for(int i=0,j=i+dif;j<n;j++,i++){
+                
                 if(i==j)t[i][j]=1;
+                
                 else if(dif==1){
                     if(s[i]==s[j])t[i][j]=2;
                     else t[i][j]=0;
                 }
+                
                 else{
                     
                     if(s[i]==s[j] && t[i+1][j-1]){
