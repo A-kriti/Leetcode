37a21fd440c2842561d20bea4484d8ada153473f
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-09-25 12:51:18 +0530

    Time: 133 ms (62.62%), Space: 10.6 MB (88.92%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -4,23 +4,27 @@ public:
         
         int n=coins.size();
         int t[n+1][amount+1];
-       
+        memset(t,-1,sizeof(t));
+        
         for(int i=0;i<=n;i++){
             for(int j=0;j<=amount;j++){
-                if(i==0)t[i][j]=INT_MAX-1;
+                
                 if(j==0)t[i][j]=0;
+                
+                if(i==0)t[i][j]=INT_MAX-1;
             }
         }
         
         for(int i=1;i<=n;i++){
             for(int j=1;j<=amount;j++){
-                if(coins[i-1]<=j)
-                    t[i][j]=min(1+t[i][j-coins[i-1]],t[i-1][j]);
-                else 
-                    t[i][j]=t[i-1][j];
+                if(coins[i-1]<=j){
+                    t[i][j]= min(1+t[i][j-coins[i-1]],t[i-1][j]);
+                }
+                
+                else t[i][j]=t[i-1][j];
             }
         }
-        return (t[n][amount]==INT_MAX-1)?-1:t[n][amount];
         
+        return (t[n][amount]==INT_MAX-1)?-1:t[n][amount];
     }
 };
