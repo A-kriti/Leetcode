ba3d71a8819ff96686bedad6d11f6e882ba5f738
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-09-28 18:53:24 +0530

    Time: 3 ms (64.89%), Space: 8.3 MB (69.91%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -4,27 +4,65 @@ public:
         
         int n=nums.size();
         
-        int left=0;
-        int mid=0;
-        int right=n-1;
+        int x1=0;
+        int x2=0;
+        int x3=0;
         
-        while(mid<=right){
-            
-            if(nums[mid]==0){
-                swap(nums[mid],nums[left]);
-                left++;
-                mid++;
-            }
-            
-            else if(nums[mid]==1){
-                mid++;
-            }
-            
-            else if(nums[mid]==2){
-                swap(nums[mid],nums[right]);
-                right--;
-            }
+        for(int i=0;i<n;i++){
+            if(nums[i]==0)x1++;
+            if(nums[i]==1)x2++;
+            if(nums[i]==2)x3++;
+        }
+        
+        int k=0;
+        while(x1){
+            nums[k]=0;
+            x1--;
+            k++;
+        }
+        
+        while(x2){
+            nums[k]=1;
+            x2--;
+            k++;
+        }
+        
+        while(x3){
+            nums[k]=2;
+            x3--;
+            k++;
         }
         
     }
 };
+
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+        
+//         int n=nums.size();
+        
+//         int left=0;
+//         int mid=0;
+//         int right=n-1;
+        
+//         while(mid<=right){
+            
+//             if(nums[mid]==0){
+//                 swap(nums[mid],nums[left]);
+//                 left++;
+//                 mid++;
+//             }
+            
+//             else if(nums[mid]==1){
+//                 mid++;
+//             }
+            
+//             else if(nums[mid]==2){
+//                 swap(nums[mid],nums[right]);
+//                 right--;
+//             }
+//         }
+        
+//     }
+// };
