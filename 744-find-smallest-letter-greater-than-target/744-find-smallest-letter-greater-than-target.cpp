5986adc46ce92a5591929be41cc383cd1f231f90
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-09-29 10:36:04 +0530

    Time: 41 ms (14.63%), Space: 15.7 MB (99.65%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
@@ -2,40 +2,29 @@ class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
         
-        //Method 1 (Binary Search)
-        
-        int t=target - '0';
         int n=letters.size();
-        int left=0;
-        int right=n;
-        string ans="";
         
-        while(left<=right){
-            int mid=(left+right)/2;
-            int y=letters[mid]-'0';
-            if(y>t){
-                ans=letters[mid];
-                right = mid-1;
+        int l=0;
+        int r=n-1;
+        int x=target;
+        int ans=0;
+        
+        while(l<=r){
+            
+            int m=(l+r)/2;
+            int k=letters[m];
+            
+            //if(k==x)return letters[m];
+            
+            if(k>x){
+                ans=m;
+                r=m-1;
             }
             
-            else{
-                left=mid+1; 
-            }
-           
+            else l=m+1;   
         }
-        if(ans.size()>0)return ans[0];
-        return letters[0];
         
-        //Method 2
-        
-//         int t=target - '0';
-        
-//         for(char x:letters){
-            
-//             int y=x-'0';
-//             if(y>t)return x;
-//         }
-        
-//         return letters[0];
+        //if(ans=-1)return letters[0];
+        return letters[ans];
     }
 };
