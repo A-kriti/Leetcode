9a16ab2805158ca4759d91ae4bd5a4d139b3cbf7
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-10-04 08:52:23 +0530

    Time: 6 ms (28.15%), Space: 9.8 MB (94.56%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -15,8 +15,9 @@ public:
         
         if(!p && !q)return true;
         
-        if((!p && q) || (p && !q) || (p->val != q->val))return false;
+        if((!p && q) || (p && !q) || (p->val!=q->val))return false;
         
         return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+        
     }
 };
