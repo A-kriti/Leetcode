25107b84ec09579987ae380c707eb93043f65019
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-10-20 10:16:23 +0530

    Time: 63 ms (5.58%), Space: 19.5 MB (92.20%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()/2];
+        
+    }
+};
+
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+        
+        
+        
+//     }
+// };
+
+
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+        
+        
+        
+//     }
+// };
