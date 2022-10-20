0675ab7260f69705c43e959704f0b83c2acaa683
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-10-20 10:26:39 +0530

    Time: 15 ms (97.95%), Space: 19.5 MB (60.12%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,3 +1,5 @@
+
+//Method 1
 // class Solution {
 // public:
 //     int majorityElement(vector<int>& nums) {
@@ -8,30 +10,48 @@
 //     }
 // };
 
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        
-        map<int,int>m;
-        int n=nums.size();
-        for(int x:nums){
-            m[x]++;
-        }
-        
-        for(auto x:m){
-            if(x.second>(n/2))return x.first;
-        }
-        
-        return -1;
-    }
-};
-
-
+//Method 2
 // class Solution {
 // public:
 //     int majorityElement(vector<int>& nums) {
         
+//         map<int,int>m;
+//         int n=nums.size();
+//         for(int x:nums){
+//             m[x]++;
+//         }
         
+//         for(auto x:m){
+//             if(x.second>(n/2))return x.first;
+//         }
         
+//         return -1;
 //     }
 // };
+
+
+//Method 3
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        int n=nums.size();
+        int v=0;
+        int c=0;
+        
+        for(int i=0;i<n;i++){
+            
+            if(v==0){
+                c=nums[i];
+            }
+            
+            if(nums[i]==c)v++;
+            
+            else{
+                v--;
+            }
+        }
+        
+        return c;
+    }
+};
